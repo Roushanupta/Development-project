7afe4738433511e3c9bdd84eafa1ac6cec1273d3
I'm Roushan Gupta <160336830+Roushanupta@users.noreply.github.com>
2025-04-25 16:41:46 +0530

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const connectDB = require('./config');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
+const projectRoutes = require('./routes/projectRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+connectDB();
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/tasks', taskRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
